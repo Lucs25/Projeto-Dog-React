08bb0e51e60d8ce8048e893c932b17df526c25e4
Lucas <lucs.25@hotmail.com>
2023-03-28 15:17:36 -0300

    envio dos arq's do projeto site para cachorros

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "./Forms/input";
+import Button from "./Forms/Button";
+import useForm from "../../Hooks/UseForm";
+import { UserContext } from "../../UserContext";
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input name="username" label="Usuário" type="text" {...username} />
+        <Input name="password" label="Senha" type="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
---
+++ src/Components/Login/LoginPassworLost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPassworLost = () => {
+  return <div>lost</div>;
+};
+
+export default LoginPassworLost;
